7c6d613ea619487bf90c413a86b3f24f31bad2db
mbochene <38912172+mbochene@users.noreply.github.com>
2019-04-24 17:51:57 +0200

    Dokończenie prototypu procedury new_fetch.

--- addedFeatures.pl
+++ addedFeatures.pl
@@ -1,16 +1,16 @@
 % pokazuje (co najwyżej) N pierwszych węzłów grafu stanów
-show_nodes(_, _, 0, 0) :- !.
+show_nodes(_, _, 0, 0) :- !.		 % ! -> w razie nawrotu koniec
 
 show_nodes([], _, N, N).
 
-show_nodes([X|R], ClosedSet, N, Rejected):-
-	is_member(X, ClosedSet), ! ,
-	show_nodes(R, ClosedSet, N, Rejected).
+show_nodes([node(State, Action,Parent, Cost, Score)|Queue], ClosedSet, N, Rejected):-
+	is_member(node(State, _, _, _, _), ClosedSet), ! ,
+	show_nodes(Queue, ClosedSet, N, Rejected).
 
-show_nodes([X|R], ClosedSet, N, Rejected):-
+show_nodes([X|Queue], ClosedSet, N, Rejected):-
 	write(X), nl,
 	NewN is N - 1,
-	show_nodes(R, ClosedSet, NewN, Rejected).
+	show_nodes(Queue, ClosedSet, NewN, Rejected).
 
 % sprawdza czy dany element należy do listy (odpowiednik procedury wbudowanej "member")
 is_member(X, [X|_]).
@@ -24,7 +24,7 @@ get_input_indexes(N, IndexList) :-
 	get_index_list(0, N, IndexList).
 
 % pobiera listę indeksów od użytkownika
-get_index_list(N, N, []).
+get_index_list(N, N, []) :- !.			% ! -> w razie nawrotu koniec (niepowodzenie)
 
 get_index_list(CurrN, N, [X|IndexList]) :-
 	NextCurrN is CurrN+1,
@@ -35,10 +35,26 @@ get_index_list(CurrN, N, [X|IndexList]) :-
 get_index(Index, IndexList) :-
 	is_member(Index, IndexList).
 
+% wrapper dla get_node_by_index_proc
+	get_node_by_index(OutNode, Queue, ClosedSet, Index) :-
+		get_node_by_index_proc(OutNode, _, Queue, ClosedSet, Index, 1).
+
+% procedura wczytująca do OutNode węzeł na zadanej przez indeks pozycji (nie wliczając węzłów nalezących do ClosedSet)
+get_node_by_index_proc(OutNode, node(State, Action,Parent, Cost, Score), [node(State, Action,Parent, Cost, Score) |Queue], ClosedSet, Index, Counter) :-
+	is_member(node(State, _ ,_  , _ , _ ) , ClosedSet),   !,
+	get_node_by_index_proc(OutNode, _, Queue, ClosedSet, Index, Counter).
+
+get_node_by_index_proc(OutNode, _, [_|Queue], ClosedSet, Index, Counter) :-
+	Index > Counter, !,
+	NewCounter is Counter+1,
+	get_node_by_index_proc(OutNode, _, Queue, ClosedSet, Index, NewCounter).
+
+get_node_by_index_proc(Node, Node, [Node|Queue], ClosedSet, Index, Index).
+
 % zmodyfikowana procedura fetch
-new_fetch(Node, Queue, ClosedSet, RestQueue, N) :-
+new_fetch(Node, Queue, ClosedSet, N) :-
 	show_nodes(Queue, ClosedSet, N, Rejected),
 	EligibleN is N-Rejected,
 	get_input_indexes(EligibleN, IndexList),
 	get_index(Index, IndexList),
-	write(Index).
+	get_node_by_index(Node, Queue, ClosedSet, Index).
