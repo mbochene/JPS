3a3e9ac7c77934644a2a09eac93beebf6d4df760
wiaderwek <t.wiaderwek@gmail.com>
2019-04-28 15:10:00 +0200

    Add files via upload
    przestrzeń stanów

---
+++ przestrzen_stanow.pl
@@ -0,0 +1,29 @@
+succ(gd, gd-ol, 170, ol).
+succ(gd, gd-wa, 377, wa).
+succ(gd, gd-by, 165, by).
+succ(ol, ol-wa, 212, wa).
+succ(by, by-wa, 270, wa).
+succ(by, by-l, 227, l).
+succ(wa, wa-l, 137, l).
+succ(wa, wa-po, 319, po).
+succ(wa, wa-bi, 193, bi).
+succ(wa, wa-lu, 166, lu).
+succ(wa, wa-ki, 178, ki).
+succ(po, po-l, 203, l).
+succ(l, l-ka, 193, ka).
+succ(ki, ki-kr, 116, kr).
+succ(ka, ka-kr, 80, kr).
+
+goal(kr).
+
+hScore(gd, 546).
+hScore(ol, 468).
+hScore(wa, 282).
+hScore(by, 408).
+hScore(po, 378).
+hScore(bi, 470).
+hScore(lu, 259).
+hScore(l, 217).
+hScore(ki, 115).
+hScore(ka, 75).
+hScore(kr, 0).
